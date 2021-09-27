b113f23ef57ef9992421ed8ef2df8809f769ff35
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2021-09-27 17:45:39 -0500

    Added the extension for inject validators and mediatr

--- Src/Services/Ordering.Api/Startup.cs
+++ Src/Services/Ordering.Api/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Ordering.Application;
 
 namespace Ordering.Api
 {
@@ -25,12 +20,13 @@ namespace Ordering.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ordering.Api", Version = "v1" });
             });
+
+            services.AddAplicationServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Src/Services/Ordering.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Ordering.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            //TODO: Add Behaviors
+            return services;
+        }
+    }
+}
